c7ecb499204ffef188af8192230d72444584de50
afarber <afarber@www.(none)>
2014-05-14 16:21:24 +0200

    Add methods: valid and fix

--- wp-city-gender/wp-city-gender.php
+++ wp-city-gender/wp-city-gender.php
@@ -29,15 +29,19 @@ if (!class_exists('WP_City_Gender')) {
 			return $links;
 		}
 
-                public static function check($str) {
-                        return (isset($str) && strlen($str) > 0 ? $str : NULL);
+                public static function valid($str) {
+                        return (isset($str) && strlen($str) > 0);
+                }
+
+                public static function fix($str) {
+                        return (WP_City_Gender::valid($str) ? $str : '');
                 }
 
                 public static function register_form() {
-                        $first_name = isset($_POST['first_name']) ? $_POST['first_name'] : '';
+                        $first_name = WP_City_Gender::fix($_POST['first_name']);
                         ?>
                         <p>
-                        <label for="first_name">First Name<br />
+                        <label for="first_name"><?php _e('First Name', 'mydomain') ?><br />
                         <input type="text" name="first_name" id="first_name" class="input" value="<?php echo esc_attr(stripslashes($first_name)); ?>" size="25" />
                         </label>
                         </p>
@@ -45,13 +49,13 @@ if (!class_exists('WP_City_Gender')) {
                 }
 
                 public static function registration_errors($errors, $sanitized_user_login, $user_email) {
-                        if (empty($_POST['first_name']))
-                                $errors->add('first_name_error', '<strong>ERROR</strong>: You must include a first name.');
+                        if (!WP_City_Gender::valid($_POST['first_name']))
+                                $errors->add('first_name_error', __('<strong>ERROR</strong>: You must include a first name.','mydomain'));
                         return $errors;
                 }
 
-                public static function user_register ($user_id) {
-                        if (isset($_POST['first_name']))
+                public static function user_register($user_id) {
+                        if (WP_City_Gender::valid($_POST['first_name']))
                                 update_user_meta($user_id, 'first_name', $_POST['first_name']);
                 }
         }
